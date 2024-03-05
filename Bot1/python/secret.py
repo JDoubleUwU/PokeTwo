239fa7ab1fa8ae4d2fc3dffbd01480394393f2bb
Joe W <125931177+JDoubleUwU@users.noreply.github.com>
2024-03-05 11:08:04 -0800

    Delete Bot1/python directory

--- Bot1/python/secret.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-def alter_config(path):
-    with open(path,'r', encoding='utf-8') as f:
-        configs = json.load(f)
-    configs["token"] = os.environ["NjI1NzQ4NzM3NTgyNDMyMjk3.XYkD7A.dW_a04Hp8UNXLANbr4LxCH88oOo"]
-    with open('config.json','w') as f:
-        f.write(json.dumps(configs,indent=3))
